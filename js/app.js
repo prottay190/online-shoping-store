d5be3d30395ab85377e6d181fff69b5c62fc848b
prottayRoy <aarnob2020@gmail.com>
2021-09-13 21:16:16 +0600

    display ditails complated

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ const showProducts = (products) => {
       <p>Average Rating: ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" onclick="loadDetails()" class="btn btn-danger">Details</button></div>
+      <button id="details-btn" onclick="loadDetails('${product.id}')" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -81,20 +81,23 @@ const updateTaxAndCharge = () => {
 
 //display details
 const loadDetails = id => {
-  const url = `https://fakestoreapi.com/products?=${id}`;
+  const url = `https://fakestoreapi.com/products/${id}`;
   fetch(url)
   .then(res => res.json())
-  .then(data => displayDetails(data[0]))
+  .then(data => displayDetails(data))
 }
 
 displayDetails = product => {
     const productDetails = document.getElementById('load-details');
+    productDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card')
     div.innerHTML = `<div class="single-product">
+    <img class="product-image" src="${product.image}">
     <h3>${product.title}</h3>
     <p>Category: ${product.category}</p>
     <p>Description: ${product.description}</p>
+    </div>
     `;
     productDetails.appendChild(div)
 }
